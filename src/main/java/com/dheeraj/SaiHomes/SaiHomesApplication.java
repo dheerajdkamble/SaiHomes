a46b37e73fcfa044f1852a4c4078eebb10400aa5
Dheeraj Kamble <dheeraj.kamble@gmail.com>
2021-07-10 13:59:01 +0530

    Initial commit

---
+++ src/main/java/com/dheeraj/SaiHomes/SaiHomesApplication.java
@@ -0,0 +1,13 @@
+package com.dheeraj.SaiHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiHomesApplication.class, args);
+	}
+
+}
